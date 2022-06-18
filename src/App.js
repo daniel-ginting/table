fe5dfb887679731799b570baa1993eab9861cb4e
Daniel Ginting <daniel_yjg@yahoo.com>
2022-06-18 11:45:41 +0800

    authentication

--- src/App.js
+++ src/App.js
@@ -2,24 +2,60 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import Table from "./components/table/table.component";
 import Form from "./components/form/form.component";
+import AuthForm from "./components/auth-form/auth-form.component";
 
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [route, setRoute] = useState("form");
 
   useEffect(() => {
     fetch("https://agile-garden-36934.herokuapp.com/students")
-    // fetch("http://localhost:3000/students")
+      // fetch("http://localhost:3000/students")
       .then((res) => res.json())
       .then((res) => {
         setData(res);
       });
   }, []);
+
+  const submitHandler = (code) => {
+    console.log(code)
+    fetch(
+      "https://agile-garden-36934.herokuapp.com/students", 
+      // "http://localhost:3000/students",
+      {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (typeof res === "object") {
+          setRoute("authenticated");
+        }
+      });
+  };
+
+  const renderRoute = () => {
+    if (route === "form") {
+      return <AuthForm submitHandler={submitHandler} />;
+    } else {
+      return (
+        <div className="main-page">
+          <h1>Students List</h1>
+          <Form setLoading={setLoading} />
+          <Table data={data} setLoading={setLoading} loading={loading} />
+        </div>
+      );
+    }
+  };
   return (
     <div className="App" style={{ cursor: loading ? "progress" : "default" }}>
-      <h1>Students List</h1>
-      <Form setLoading={setLoading}/>
-      <Table data={data} setLoading={setLoading} loading={loading}/>
+      {renderRoute()}
     </div>
   );
 }
---
+++ src/components/auth-form/auth-form.component.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import "./auth-form.styles.scss";
+
+const AuthForm = ({ submitHandler }) => {
+  const [code, setCode] = useState("");
+
+  const onChangeHandler = (event) => {
+    console.log(event.target.value)
+    setCode(event.target.value);
+  };
+
+  const onSubmitHandler = () => {
+    submitHandler(code);
+  };
+  return (
+    <div className="form">
+      <div className="title">Welcome!</div>
+      <div className="subtitle">Fill in the secret code</div>
+      <div className="input-container ic1">
+        <input
+          id="firstname"
+          className="input"
+          type="text"
+          placeholder=" "
+          onChange={onChangeHandler}
+        />
+        <div className="cut"></div>
+        <label htmlFor="firstname" className="placeholder">
+          Secret code
+        </label>
+      </div>
+      <button type="text" className="submit" onClick={onSubmitHandler}>
+        Authenticate
+      </button>
+    </div>
+  );
+};
+
+export default AuthForm;
