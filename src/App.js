0cca0f7c93a82219b4e36dba9b7be88dd9fef85e
Daniel Ginting <daniel_yjg@yahoo.com>
2022-06-21 12:38:48 +0700

    sk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Table from "./components/table/table.component";
 import Form from "./components/form/form.component";
 import AuthForm from "./components/auth-form/auth-form.component";
@@ -20,37 +20,66 @@ function App() {
 
   const submitHandler = (code) => {
     fetch(
-      "https://students-list-server.herokuapp.com/students", 
+      "https://students-list-server.herokuapp.com/students",
       // "http://localhost:3000/students",
       {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        code,
-      }),
-    })
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          code,
+        }),
+      }
+    )
       .then((res) => res.json())
       .then((res) => {
         if (typeof res === "object") {
           setData(res);
           setRoute("authenticated");
+          window.sessionStorage.setItem("authenticated", true)
         }
       });
   };
 
+  useEffect(() => {
+    fetch(
+      "https://students-list-server.herokuapp.com/students",
+      // "http://localhost:3000/students",
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          code: '1234',
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (typeof res === "object") {
+          setData(res);
+        }
+      });
+  }, [])
+
   const renderRoute = () => {
-    if (route === "form") {
-      return <AuthForm submitHandler={submitHandler} />;
-    } else {
+    if (
+      route === "authenticated" ||
+      window.sessionStorage.getItem("authenticated")
+    ) {
+      
       return (
         <div className="main-page">
           <h1>Students List</h1>
           <Form setLoading={setLoading} />
           <Table data={data} setLoading={setLoading} loading={loading} />
+          {console.log(1234)}
         </div>
       );
+    } else if (route === "form") {
+      return <AuthForm submitHandler={submitHandler} />;
     }
   };
   return (
